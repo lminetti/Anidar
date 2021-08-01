d664e49cea98fbc527ebef0bc01fe0153aa15491
Luciano <luciano.minetti@gmail.com>
2021-08-01 18:14:07 -0300

    Agregar archivos de proyecto.

---
+++ Anidar/Controllers/PersonasController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Anidar.Models;
+
+namespace Anidar.Controllers
+{
+    public class PersonasController : Controller
+    {
+        //[HttpGet]
+        public ActionResult AddOrEdit(int id=0)
+        {
+            Persona personaModel = new Persona();
+            return View(personaModel);
+        }
+
+        //[HttpPost]
+        //public ActionResult AddOrEdit(Persona personaModel)            
+        //{
+        //    using (AnidarEntities dbModel = new AnidarEntities())
+        //    {
+        //        dbModel.Personas.Add(personaModel);
+        //        dbModel.SaveChanges();
+        //    }
+        //    ModelState.Clear();
+        //    ViewBag.SuccessMessage = "Ingreso correcto.";
+        //    return View("AddOrEdit", new Persona());
+
+        //}
+    }
+}
---
+++ Anidar/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Anidar.MvcApplication" Language="C#" %>
