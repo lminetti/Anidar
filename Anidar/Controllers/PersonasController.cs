4e125be2330f9289fc769a6f6aaee5be0c1cfeec
Luciano <luciano.minetti@gmail.com>
2021-08-02 16:40:23 -0300

    Editado

--- Anidar/Controllers/PersonasController.cs
+++ Anidar/Controllers/PersonasController.cs
@@ -9,25 +9,25 @@ namespace Anidar.Controllers
 {
     public class PersonasController : Controller
     {
-        //[HttpGet]
+        [HttpGet]
         public ActionResult AddOrEdit(int id=0)
         {
             Persona personaModel = new Persona();
             return View(personaModel);
         }
 
-        //[HttpPost]
-        //public ActionResult AddOrEdit(Persona personaModel)            
-        //{
-        //    using (AnidarEntities dbModel = new AnidarEntities())
-        //    {
-        //        dbModel.Personas.Add(personaModel);
-        //        dbModel.SaveChanges();
-        //    }
-        //    ModelState.Clear();
-        //    ViewBag.SuccessMessage = "Ingreso correcto.";
-        //    return View("AddOrEdit", new Persona());
+        [HttpPost]
+        public ActionResult AddOrEdit(Persona personaModel)
+        {
+            using (AnidarEntities dbModel = new AnidarEntities())
+            {
+                dbModel.Personas.Add(personaModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Ingreso correcto.";
+            return View("AddOrEdit", new Persona());
 
-        //}
+        }
     }
 }
